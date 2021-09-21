65469970dd645412b04544ab45f15bd994126e28
Alex White <alexlloydwhite@gmail.com>
2021-09-21 14:34:13 -0500

    adding DAO

---
+++ src/main/java/com/example/demo/dao/PersonDAO.java
@@ -0,0 +1,15 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+
+import java.util.UUID;
+
+public interface PersonDAO {
+
+    int insertPerson(UUID id, Person person);
+
+    default int addPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+}

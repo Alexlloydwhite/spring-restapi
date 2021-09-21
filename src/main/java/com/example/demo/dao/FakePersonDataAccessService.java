e50aa8cb8754bdbcc17fdc93590f8d9fde4c72c7
Alex White <alexlloydwhite@gmail.com>
2021-09-21 15:02:11 -0500

    alskjhdjfal;kjsdl;fkjas;ldkfja;lsjkdf  fucki jaca:

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -17,4 +17,10 @@ public class FakePersonDataAccessService implements PersonDAO {
         DB.add(new Person(id, person.getName()));
         return 1;
     }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return null;
+    }
+
 }
--- src/main/java/com/example/demo/dao/PersonDAO.java
+++ src/main/java/com/example/demo/dao/PersonDAO.java
@@ -2,6 +2,7 @@ package com.example.demo.dao;
 
 import com.example.demo.model.Person;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PersonDAO {
@@ -12,4 +13,6 @@ public interface PersonDAO {
         UUID id = UUID.randomUUID();
         return insertPerson(id, person);
     }
+
+    List<Person> selectAllPeople();
 }

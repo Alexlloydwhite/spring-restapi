50f6989f2cdbc8be23f42d4052df7c968c6210a0
Alex White <alexlloydwhite@gmail.com>
2021-09-21 14:44:28 -0500

    adding dep injections

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -2,11 +2,13 @@ package com.example.demo.api;
 
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class PersonController {
 
     private final PersonService personService;
 
+    @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -2,12 +2,17 @@ package com.example.demo.service;
 
 import com.example.demo.dao.PersonDAO;
 import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PersonService {
 
     private final PersonDAO personDAO;
 
-    public PersonService(PersonDAO personDAO) {
+    @Autowired
+    public PersonService(@Qualifier("fakeDAO") PersonDAO personDAO) {
         this.personDAO = personDAO;
     }
 

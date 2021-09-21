d55174c859f13ae7474184850ee8919705363e63
Alex White <alexlloydwhite@gmail.com>
2021-09-21 14:54:57 -0500

    adding mapping to API

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,7 +3,13 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/v1/person")
+@RestController
 public class PersonController {
 
     private final PersonService personService;
@@ -13,7 +19,8 @@ public class PersonController {
         this.personService = personService;
     }
 
-    public void addPerson(Person person) {
+    @PostMapping
+    public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
     }
 }
